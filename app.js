2f0b6068ddce36925b64dc5d20724db135fabd82
friendlymoose5717 <friendlymoose5717@gmail.com>
2026-05-20 17:24:41 +0200

    Update app.js
    Remove text outgoing delegations.

--- app.js
+++ app.js
@@ -294,8 +294,7 @@ dash.innerHTML = `
       <h3>Incoming downvotes (30d)</h3>
       <div id="downvoteTable"></div>
 
-      <h3>Outgoing delegations</h3>
-      <div id="delegationTable"></div>
+
   `;
 
 // Color rules
