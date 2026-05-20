0e6cb62612bc1f995c615db57b920384b4f51ebc
friendlymoose5717 <friendlymoose5717@gmail.com>
2026-05-20 17:28:00 +0200

    Update app.js
    KE colors fixed

--- app.js
+++ app.js
@@ -309,9 +309,6 @@ setCard("blacklistCard", isBL ? "YES" : "NO", isBL ? "danger" : "ok");
 
 // KE color rules
 const keStatus =
-    ke.krampus < 0 ? "danger" :
-    ke.krampus < 0.001 ? "warning" :
-    "ok";
     ke.krampus < 2 ? "ok" :
     ke.krampus < 5 ? "warning" :
     "danger";
